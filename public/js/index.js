788d109a7f623f0ac27566dbd1d09e5ee81eac6b
Miia Laitinen <miia.laitinen@hotmail.com>
2019-02-03 17:26:52 +0200

    initial commit

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+
+// open sign in form
+$("#sign-in-link").click(function(){
+  $("#sign-in").show();
+  $("#register").hide();
+  $("#x").addClass("focus-link");
+  $("#y").removeClass("focus-link");
+  $("#sign-in-link").addClass("disabled").addClass("disabled-link");
+  $("#register-link").removeClass("disabled").removeClass("disabled-link");
+});
+
+// open register form
+$("#register-link").click(function(){
+  $("#register").show();
+  $("#sign-in").hide();
+  $("#y").addClass("focus-link");
+  $("#x").removeClass("focus-link");
+  $("#register-link").addClass("disabled").addClass("disabled-link");
+  $("#sign-in-link").removeClass("disabled").removeClass("disabled-link");
+});
